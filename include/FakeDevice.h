9f5ede1060e0b1e25736aeaba37eeb4a114607d0
kn65op <kn65op@gmail.com>
2012-04-23 22:35:36 +0200

    Added FakeDevice

---
+++ include/FakeDevice.h
@@ -0,0 +1,45 @@
+/* 
+ * File:   FakeDevice.h
+ * Author: tomko
+ *
+ * Created on 23 kwiecień 2012, 21:18
+ */
+
+#ifndef FAKEDEVICE_H
+#define	FAKEDEVICE_H
+
+#include <Device.h>
+
+#include <fstream>
+
+class FakeDevice : public Device
+{
+public:
+  FakeDevice();
+  FakeDevice(const FakeDevice& orig);
+  virtual ~FakeDevice();
+
+  void sendChar(char to_send) throw(Device::ConnectionError)
+  {
+
+  }
+
+  void startConnection()
+  {
+
+  }
+
+  void stopConnection()
+  {
+    if (inf.is_open())
+    {
+      inf.close();
+    }
+  }
+  u_int32_t receiveUInt4() throw (Device::ConnectionError);
+private:
+  std::ifstream inf;
+};
+
+#endif	/* FAKEDEVICE_H */
+
---
+++ src/FakeDevice.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   FakeDevice.cpp
+ * Author: tomko
+ * 
+ * Created on 23 kwiecień 2012, 21:18
+ */
+
+#include "../include/FakeDevice.h"
+
+
+FakeDevice::FakeDevice() : Device()
+{
+}
+
+FakeDevice::FakeDevice(const FakeDevice& orig) : Device((Device)(orig))
+{
+}
+
+FakeDevice::~FakeDevice()
+{
+}
+
+u_int32_t FakeDevice::receiveUInt4() throw (Device::ConnectionError)
+{
+  sleep(1);
+  inf.open("fake.txt", std::ios::in);
+  if (!inf.eof())
+  {
+    u_int32_t tmp;
+    inf >> tmp;
+    return tmp;
+  }
+  else
+  {
+    inf.close();
+    inf.open("fake.txt", std::ios::in);
+  }
+}
